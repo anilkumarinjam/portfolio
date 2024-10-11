7a767745a20b813b702f68a0bd03e2daf266d613
anilkumarinjam <injamanilchowdary@gmail.com>
2024-10-11 17:52:06 -0500

    hamburger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUser, faProjectDiagram, faEnvelope, faArrowDown } from '@fortawesome/free-solid-svg-icons';
 import '../css/Navbar.css';
 
 const Navbar = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
   return (
     <nav className="sidebar">
       <div className="initials-div">
         AK
       </div>
-      
-      <div className="nav-links-container">
+
+      <div className={`nav-links-container ${menuVisible ? 'visible' : ''}`}>
         <Link className="nav-link" to="/">
           <div className="icon"><FontAwesomeIcon icon={faHome} /></div>
           <span>Home</span>
@@ -29,8 +35,14 @@ const Navbar = () => {
           <span>Contact</span>
         </Link>
       </div>
+
       <div className="scroll-arrow">
         <FontAwesomeIcon icon={faArrowDown} />
+      </div>
+
+      {/* Toggle button */}
+      <div className="menu-icon" onClick={toggleMenu}>
+        &#9776;
       </div>
     </nav>
   );
