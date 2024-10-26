be19ee394a7080f88dba2fbdd4eafe7ccc022f4a
anilkumarinjam <injamanilchowdary@gmail.com>
2024-10-25 22:25:03 -0500

    mobile render

--- src/App.js
+++ src/App.js
@@ -6,30 +6,6 @@ import './css/App.css';
 import './css/globals.css';
 
 function App() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  // Use effect to detect screen size on load
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768); // Set true if screen width is <= 768px (mobile devices)
-    };
-
-    handleResize(); // Initial check
-    window.addEventListener('resize', handleResize); // Listen for window resize
-
-    return () => {
-      window.removeEventListener('resize', handleResize); // Clean up the listener
-    };
-  }, []);
-
-  if (isMobile) {
-    return (
-      <div className="mobile-warning">
-        This application cannot be rendered on mobile devices. Please view it on a desktop browser.
-      </div>
-    );
-  }
-
   return (
     <Router basename="/portfolio">
       <Navbar />
